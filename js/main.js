e583fce0f1c8d59c3790119356ed74a84a5f2d9f
Thomas <mirandabthomas@gmail.com>
2026-07-18 23:54:26 -0300

    commit final de la actividad

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@
 alert("Bienvenido a mi sitio web!");
 
 // Captura de datos personales del usuario
-let nombreUsuario = prompt("¿Como te llamas?");
+const nombreUsuario = prompt("¿Como te llamas?");
 console.log(nombreUsuario);
-let edadUsuario = prompt("¿Cuantos años tenes?");
+const edadUsuario = prompt("¿Cuantos años tenes?");
 edadUsuario = parseInt(edadUsuario);
 console.log(edadUsuario);
 
 // Captura y conversión de datos para operación matemática
-let numero1 = parseInt(prompt("Introduzca el primer numero que desea sumar"));
-let numero2 = parseInt(prompt("Introduzca el segundo numero que desea sumar:"));
+const numero1 = parseInt(prompt("Introduzca el primer numero que desea sumar"));
+const numero2 = parseInt(prompt("Introduzca el segundo numero que desea sumar:"));
 
 // Procesamiento aritmético de los datos ingresados
-let calculo = numero1 + numero2;
+const calculo = numero1 + numero2;
 console.log(calculo);
 
 // Transformación de texto y salida de datos
